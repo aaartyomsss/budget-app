6ad8f22c68a4308f924fe1aa7847db11e13b4a13
Artjom Savin <artjom.savin@nordhealth.com>
2023-07-11 07:57:33 +0300

    Nothing has changed

--- backend/elasticSearch/index.js
+++ backend/elasticSearch/index.js
@@ -1,6 +1,7 @@
-const elasticsearch = require('elasticsearch');
+const elasticsearch = require('elasticsearch')
+
 const ESclient = new elasticsearch.Client({
   host: process.env.ELASTICSEARCH_HOST,
-});
+})
 
-module.exports = ESclient;
+module.exports = ESclient
